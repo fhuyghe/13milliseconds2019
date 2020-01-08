699fbb08fff2f503db919590bc596d55fb5b12aa
Francois Huyghe <francois@13milliseconds.com>
2020-01-08 13:32:56 -0600

    404 page

--- web/app/themes/13milliseconds2019/resources/views/404.blade.php
+++ web/app/themes/13milliseconds2019/resources/views/404.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
+  <div class="container">
   @if (!have_posts())
-    <div class="alert alert-warning">
+    <img class="gif-404" src="@asset('images/travolta-lost.gif')" />
+    <h1>Error 404</h1>
+    <p>
       {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
+    </p>
+      <a class="btn" href="/">Back to work</a>
   @endif
+  </div>
 @endsection
